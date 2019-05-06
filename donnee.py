9756d45cac6660c195a96a93b2a628ca9b56dc3a
marycha64800 <50314698+marycha64800@users.noreply.github.com>
2019-05-06 08:31:31 +0200

    Add files via upload

---
+++ donnee.py
@@ -0,0 +1,7 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+scoreboard = dict()
+number_word_in_file = 323577
+
---
+++ pendu.py
@@ -0,0 +1,16 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import fonctions
+
+
+def launch_game():
+    """ Lancement de la partie """
+    while fonctions.course_of_the_game() is not None:
+        pass
+
+launch_game()
+
+
+
